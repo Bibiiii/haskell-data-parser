0584bd78d9ffa6d45add4b9b7bbc0a716591d4fa
Luca133 <40567382+Luca133@users.noreply.github.com>
2020-12-07 12:23:40 +0000

    Test Queries

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,12 @@
 module Main where
 
 import ProcessJson
+import Queries
 
 main :: IO ()
 main = do
     processJson "https://swapi.dev/api/" "root"
     print "Parsing..."
     print "Done!"
+
+    query 5
