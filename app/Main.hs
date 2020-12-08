4796d2115c2d7ed48d5e2711668ebdee3eb75209
Luca133 <40567382+Luca133@users.noreply.github.com>
2020-12-08 13:04:13 +0000

    Final Query Set
    Added drop all tables
    Added find planet with the highest ground

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,7 @@ module Main where
 import ProcessJson
 import Queries
 import ProcessJson ( processJson )
-import Queries ( queryJoin, queryDelete, dropAllTables) 
+import Queries ( dropAllTables, queryHomeworlds, queryDelete, queryPlanetWithHighestGround) 
 
 main :: IO ()
 main = do
@@ -11,22 +11,28 @@ main = do
     print "Dropping all tables....."
     dropAllTables
     print "All tables dropped"
-    print "------------------------"
+    print "---------------------------------------------"
 
     processJson "https://swapi.dev/api/" "root"
     print "Parsing..."
     print "Done!"
 
-    print "Performing first query - First 10 people and their home planets....."
-    queryJoin
-    print "Done with first query!"
-    print "------------------------"
+    print "Performing homeworlds query - First 10 people and their home planets....."
+    queryHomeworlds
+    print "Done with homeworlds query!"
+    print "---------------------------------------------"
 
-    print "Performing delete query....."
+    print "Performing delete query - Delete all people from Tatooine....."
     queryDelete
     print "Done with delete query!"
-    print "------------------------"
+    print "---------------------------------------------"
 
-    print "Testing delete query....."
-    queryJoin
+    print "Testing delete query by performing homeworlds query again....."
+    queryHomeworlds
+    print "Done testing delete query by repeating homeworlds query!"
+    print "---------------------------------------------"
 
+    print "Planet with the highest ground...."
+    queryPlanetWithHighestGround
+    print "Found planet with highest ground"
+    print "---------------------------------------------"
