966b5ddfbcc1e3f29c9a8387f8d9ad6e3e69037f
Luca133 <40567382+Luca133@users.noreply.github.com>
2020-12-08 11:13:29 +0000

    Updated Queries

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,38 @@
 module Main where
 
+<<<<<<< Updated upstream
 import ProcessJson
 import Queries
+=======
+import ProcessJson ( processJson )
+import Queries ( queryJoin, queryDelete, dropAllTables) 
+>>>>>>> Stashed changes
 
 main :: IO ()
 main = do
+
+    print "Dropping all tables....."
+    dropAllTables
+    print "All tables dropped"
+    print "------------------------"
+
     processJson "https://swapi.dev/api/" "root"
     print "Parsing..."
     print "Done!"
 
+<<<<<<< Updated upstream
     query 5
+=======
+    print "Performing first query - First 10 people and their home planets....."
+    queryJoin
+    print "Done with first query!"
+    print "------------------------"
+
+    print "Performing delete query....."
+    queryDelete
+    print "Done with delete query!"
+    print "------------------------"
+
+    print "Testing delete query....."
+    queryJoin
+>>>>>>> Stashed changes
--- src/Queries.hs
+++ src/Queries.hs
@@ -1,5 +1,6 @@
 module Queries
     (
+<<<<<<< Updated upstream
         query,
     ) where
 
@@ -58,3 +59,77 @@ query maxId =
 
 --        -- And disconnect from the database
 --        disconnect conn
+=======
+        queryJoin,
+        queryDelete,
+        dropAllTables
+    ) where
+
+import Database.HDBC
+    ( fromSql, quickQuery', SqlValue, IConnection(disconnect, commit) )
+
+import Database.HDBC.PostgreSQL ( connectPostgreSQL )
+
+getVal :: SqlValue -> String
+getVal stringOutput = fromSql stringOutput :: String
+
+formatStrings :: [SqlValue] -> [String]
+formatStrings = map getVal
+
+combine :: [[Char]] -> [[Char]] -> [[Char]]
+combine [] _ = []
+combine _ [] = []
+combine (x:xs) (y:ys) = (x ++ ": " ++ y) : combine xs ys
+
+dropAllTables :: IO()
+dropAllTables = 
+    do
+        conn <- connectPostgreSQL "host=localhost dbname=postgres user=postgres password=admin"
+        quickQuery' conn
+             "DROP TABLE IF EXISTS planets CASCADE"
+             []
+
+        quickQuery' conn
+             "DROP TABLE IF EXISTS people CASCADE"
+             []
+
+        quickQuery' conn
+             "DROP TABLE IF EXISTS species CASCADE"
+             []
+        
+        quickQuery' conn
+             "DROP TABLE IF EXISTS films CASCADE"
+             []
+
+        commit conn
+
+        -- And disconnect from the database
+        disconnect conn
+
+queryJoin :: IO ()
+queryJoin =
+    do
+        conn <- connectPostgreSQL "host=localhost dbname=postgres user=postgres password=admin"
+        r <- quickQuery' conn
+             "SELECT people.name, planets.name FROM people INNER JOIN planets ON people.homeworld=planets.planet_id WHERE people.person_id <=10"
+             []
+        let b = mapM formatStrings r
+        mapM_ putStrLn $ combine (head b) (last b)
+        disconnect conn
+
+
+queryDelete :: IO ()
+queryDelete = 
+    do -- Connect to the database
+        conn <- connectPostgreSQL "host=localhost dbname=postgres user=postgres password=admin"
+
+        -- Run the query and store the results in r
+        quickQuery' conn
+            "DELETE FROM people WHERE person_id = 1"
+            []
+
+        commit conn
+
+        -- And disconnect from the database
+        disconnect conn
+>>>>>>> Stashed changes
